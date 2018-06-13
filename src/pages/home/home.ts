69951350aa021f5b8e9ae068d941b8fc91d4dbde
Brepe <bre.perdo@gmail.com>
2018-06-13 02:13:10 -0300

    Home page tratamento de erros completo. Interface melhorada.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 
 import { MapProxPage } from '../Map-prox/Map-prox';
 import { Novoevento2Page } from '../novoevento2/novoevento2';
 import { listaProxPage } from '../lista-prox/lista-prox';
 import { CadastroPage } from '../cadastro/cadastro';
-import {  Userclass } from '../../app/providers/user';
+import { Userclass } from '../../app/providers/user';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';  //<<<<por causa do erro do ngmodule nos cadastros
 import { CommonModule } from '@angular/common'; //<<<<por causa do erro do ngmodule nos cadastros
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { SugerirPage } from '../sugerir/sugerir';
 import { Observable } from 'rxjs/Observable'; //para o auth firebase ngif
 import * as firebase from 'Firebase';
-import { MeuseventosPage } from '../meuseventos/meuseventos';
-
 
 @Component({
   selector: 'page-home',
@@ -22,88 +20,72 @@ import { MeuseventosPage } from '../meuseventos/meuseventos';
 export class HomePage {
   usuario = {} as Userclass;
   use: Observable<firebase.User>; //para o auth firebase ngif
-  private msgErro : any;
+  private msgErro: any;
 
-  constructor(public navCtrl: NavController, public afauth: AngularFireAuth,     private toast: ToastController) {
+  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public afauth: AngularFireAuth, public loadingCtrl: LoadingController) {
     this.use = afauth.authState; //para o auth firebase ngif
 
   }
-  
+
   logout() {
     return this.afauth.auth.signOut();
-    
+
   }
 
   ngOnInit() {
-    this.usuario = {} as Userclass;
 
-    firebase.auth().onAuthStateChanged(function(use) {
-      if (use) {
-        console.log(" User is signed in.");
-      } else {
-        console.log("No user is signed in.");
-      }
+    try {
+      this.load();
+      this.usuario = {} as Userclass;
+
+      firebase.auth().onAuthStateChanged(function (use) {
+        if (use) {
+          console.log(" User is signed in.");
+        } else {
+          console.log("No user is signed in.");
+        }
+      });
+    } catch (e) {
+      this.navCtrl.setRoot(HomePage);
+      console.log(e);
+    }
+  }
+  load() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'dots'
     });
+    loading.present();
+    setTimeout(() => {
+      loading.dismiss();
+    }, 1500);
   }
 
-  // ionViewWillLoad(){
+  async login(usuario: Userclass) {
+    try {
+      this.afauth.auth.signInAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.password)
+        .then(user => {
+          const result = this.afauth.auth.signInAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.password);
+          if (result) {
+            console.log(result);
+            this.navCtrl.setRoot(HomePage);
+          }
 
-  // }
-
-  async login(usuario: Userclass){
-    try{
-    const result = this.afauth.auth.signInAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.password);
-    if (result){
-      console.log(result);
-    this.navCtrl.setRoot(MeuseventosPage);
+        }, error => {
+          let alert = this.alertCtrl.create({
+            title: 'Usuário e/ou senha incorretos.',
+            buttons: ['OK']
+          });
+          alert.present();
+        })
+    } catch (e) {
+      let alert = this.alertCtrl.create({
+        title: 'Digite login e senha.',
+        buttons: ['OK']
+      });
+      alert.present();
+      console.error(e);
     }
-  
-    }catch(e){
-      this.toast.create({ message: 'Digite corretamente login e senha.', duration: 3000 }).present();
-
-    console.error(e);
-
-    }
-    ////////////////////////////////////////////////////
-    // let _this = this;
-
-    // firebase.auth().signInAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.password)
-    // .catch(function(err : firebase.FirebaseError) {
-    //      if (err.code){
-    //           if (err.code === 'auth/weak-password') {
-    //              _this.msgErro = "A senha deve conter no mínimo 6 caracteres";
-    //          }
-    //          else if(err.code === 'auth/invalid-email'){
-    //             _this.msgErro = "O email informado é invalido";
-    //          }
-    //          else {
-    //              _this.msgErro = "O email informado ja está cadastrado";
-    //          }
-    //      }
-    //  });
   }
 
-  
-  goToNovoevento2(params){
-    if (!params) params = {};
-    this.navCtrl.push(Novoevento2Page);
-  }
-  goToMapProx(params){
-    if (!params) params = {};
-   this.navCtrl.push(MapProxPage);
-   console.log(params);
-  }
-  
-  goTolistaProx(params){
-    if (!params) params = {};
-    this.navCtrl.push(listaProxPage);
-  }
-  goToCadastro(params){
-    if (!params) params = {};
-    this.navCtrl.push(CadastroPage);
-  }
-  goToHome(params){
-    if (!params) params = {};
-    this.navCtrl.push(HomePage);
-  }
+
 }
